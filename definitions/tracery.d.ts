aa2aba45646396a7b6bed11a95279ca16bac8cf8
Ahmed Abdel Samea Khalifa <amidos2002@hotmail.com>
2017-03-17 18:47:33 -0400

    Adding

---
+++ definitions/tracery.d.ts
@@ -0,0 +1,23 @@
+declare module tracery{
+    export class TraceryNode{
+        depth:number;
+        childIndex:number;
+        parent:TraceryNode;
+        children:TraceryNode[];
+        finishedText:string;
+
+        toString():string;
+    }
+
+    export class Grammar{
+        clearState():void;
+        addModifiers(mods:{}):void;
+        expand(rule:string, allowEscapeChars:boolean):TraceryNode;
+        flatten(rule:string, allowEscapeChars:boolean):string;
+        toJSON():string;
+        toHTML():string;
+    }
+
+    export function createGrammar(raw:{}):Grammar;
+    export function setRng(newRng:()=>number):void;
+}
